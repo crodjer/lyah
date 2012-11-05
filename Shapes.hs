f1743c3bf99e72694153a49ff741eb457c59641f
Rohan Jain <crodjer@gmail.com>
2012-11-05 21:20:43 +0530

    Shapes class

--- Shapes.hs
+++ Shapes.hs
@@ -1,26 +1,35 @@
-module Shapes
-( Point(..)
-, Shape(..)
-, surface
-, nudge
-, baseCircle
-, baseRectangle
-) where
+{-# LANGUAGE ExistentialQuantification #-}
 
+import Data.Monoid
 
-data Point = Point Float Float deriving (Show)
-data Shape = Circle Point Float | Rectangle Point Point deriving (Show)
+newtype Rectangle a = Rectangle (a, a) deriving Show
+newtype Circle a = Circle a deriving Show
+data AnyShape a = forall s a. (Shape s, Floating a, Show (s a)) => AnyShape (s a)
 
-surface :: Shape -> Float
-surface (Circle _ r) = pi * r ^ 2
-surface (Rectangle (Point x1 y1) (Point x2 y2)) = (abs $ x2 - x1) * (abs $ y2 - y1)
+class Shape shape where
+  area :: (Floating a) => shape a -> a
+  perimeter :: (Floating a) => shape a -> a
 
-nudge :: Shape -> Float -> Float -> Shape
-nudge (Circle (Point x y) r) a b = Circle (Point (x+a) (y+b)) r
-nudge (Rectangle (Point x1 y1) (Point x2 y2)) a b = Rectangle (Point (x1+a) (y1+b)) (Point (x2+a) (y2+b))
+instance Shape Rectangle where
+  area (Rectangle (l, b)) = l * b
+  perimeter (Rectangle (l,  b)) = 2 * (l + b)
 
-baseCircle :: Float -> Shape
-baseCircle r = Circle (Point 0 0) r
+instance Shape Circle where
+  area (Circle r) = pi * r * r
+  perimeter (Circle r) = 2 * pi * r
 
-baseRectangle :: Float -> Float -> Shape
-baseRectangle x y = Rectangle (Point 0 0) (Point x y)
+instance Show (AnyShape a) where
+  show (AnyShape sa) = "Shape " ++ show sa
+
+-- anyShapeArea :: (Shape s, Num a, Show (s a)) => AnyShape a -> s a
+anyShapeArea (AnyShape sa) =  (area sa)
+
+-- For AnyShape to be of any use, ^ should work. I can't extract anything
+-- useful out of it.
+
+-- instance Shape (AnyShape) where
+--   area (AnyShape sa) = area sa
+--   perimeter (AnyShape sa) = area sa
+
+shapes :: Floating a => [AnyShape a]
+shapes = [AnyShape (Circle 3), AnyShape (Rectangle (2, 2))]
