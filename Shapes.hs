56a4a356256f64e074890fc29b2e27ff739247c1
Rohan Jain <crodjer@gmail.com>
2012-11-05 22:09:38 +0530

    Clean shapes basic implementation

--- Shapes.hs
+++ Shapes.hs
@@ -1,10 +1,12 @@
-{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE GADTs #-}
 
 import Data.Monoid
 
 newtype Rectangle a = Rectangle (a, a) deriving Show
 newtype Circle a = Circle a deriving Show
-data AnyShape a = forall s a. (Shape s, Floating a, Show (s a)) => AnyShape (s a)
+data AnyShape a where
+  AnyShape :: (Shape s, Floating a, Show (s a)) => s a -> AnyShape a
+newtype Shapes a = Shapes [AnyShape a] deriving Show
 
 class Shape shape where
   area :: (Floating a) => shape a -> a
@@ -21,15 +23,13 @@ instance Shape Circle where
 instance Show (AnyShape a) where
   show (AnyShape sa) = "Shape " ++ show sa
 
--- anyShapeArea :: (Shape s, Num a, Show (s a)) => AnyShape a -> s a
-anyShapeArea (AnyShape sa) =  (area sa)
+instance Shape AnyShape where
+  area (AnyShape sa) = area sa
+  perimeter (AnyShape sa) = area sa
 
--- For AnyShape to be of any use, ^ should work. I can't extract anything
--- useful out of it.
+instance Shape Shapes where
+  area (Shapes ss) = sum(map area ss)
+  perimeter (Shapes ss) = sum(map perimeter ss)
 
--- instance Shape (AnyShape) where
---   area (AnyShape sa) = area sa
---   perimeter (AnyShape sa) = area sa
-
-shapes :: Floating a => [AnyShape a]
-shapes = [AnyShape (Circle 3), AnyShape (Rectangle (2, 2))]
+shapes :: (Floating a, Show a) => Shapes a
+shapes = Shapes [AnyShape (Circle 3), AnyShape (Rectangle (2, 2))]
