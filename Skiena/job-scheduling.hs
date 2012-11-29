d3e0e915c802232244194166064e4145de49a37f
Rohan Jain <crodjer@gmail.com>
2012-11-29 15:48:27 +0530

    Exhaustive (slow) job scheduling

--- Skiena/job-scheduling.hs
+++ Skiena/job-scheduling.hs
@@ -9,8 +9,8 @@ import Data.Maybe (fromJust)
 jobRange :: (Int, Int)
 jobRange = (1, 30)
 
+-- newtype Job a = Job (a, a) deriving Show
 newtype Job a = Job (a, a)
-
 -- Print job nicely over a timeline (probably a time waste)
 instance (Show a, Integral a) => Show (Job a) where
   show (Job (start, end)) = "Job: " ++ (show (start, end)) ++ "\t|" ++ head ++ range ++ tail ++ "|\n" where
@@ -53,23 +53,45 @@ baseFilter :: (Ord a) => Maybe (Job a) -> [Job a] -> [Job a]
 baseFilter (Just (Job (s, e))) = filter (\(Job (s', e')) -> s' > e)
 baseFilter Nothing = id
 
-jobSample = randomJobs 100 10
-
 ----------------
 -- Job selectors 
 ----------------
+startSort, endSort :: (Ord a) => Job a -> Job a -> Ordering
+startSort j1@(Job (s1, e1)) j2@(Job (s2, e2))
+  | s1 < s2 = LT
+  | s1 == s2 && e1 < e2 = LT
+  | j1 == j2 = EQ
+  | otherwise = GT
 
--- exhaustiveSel :: (Ord a) => [Job a] -> [Job a]
--- exhaustiveSel = filter where
--- I'll need a tree here, comming up!
+endSort j1@(Job (s1, e1)) j2@(Job (s2, e2))
+  | e1 < e2 = LT
+  | e1 == e2 && s1 > s2 = LT -- Look for jobs starting latest (I wanna work for the least duration :P)
+  | j1 == j2 = EQ
+  | otherwise = GT
+
+
+validSel :: (Ord a) => [Job a] -> Maybe [Job a]
+validSel = (foldr foldingFunction (Just [])).sortBy startSort where
+  -- Works only with foldr
+  foldingFunction _ Nothing = Nothing
+  foldingFunction j (Just []) = Just [j]
+  foldingFunction j@(Job (s', e')) acc@(Just (Job (s, e):_))
+                   | s >= e' = fmap (j:) acc
+                   | otherwise = Nothing
+
+-- Becomes unusable around early 20s
+exhaustiveSel :: (Ord a) => [Job a] -> [Job a]
+exhaustiveSel = head.(sortBy lengthSort).validSubsets.sortBy startSort where
+  lengthSort set1 set2 = compare (length set2) (length set1)
+  validSubset selection = case (validSel selection) of
+    Nothing  -> False
+    (Just _) -> True
+  validSubsets jobs = filter validSubset $ filterM (\_ -> [True, False]) jobs
 
 -- endFirstSel: Selector which prefers jobs ending first (optimal)
 endFirstSel :: (Ord a) => [Job a] -> [Job a]
-endFirstSel = filter.sortBy endSort where
-  filter (current:jobs') = current:(filter $ baseFilter (Just current) jobs')
-  filter [] = []
-  endSort j1@(Job (s1, e1)) j2@(Job (s2, e2))
-    | e1 < e2 = LT
-    | e1 == e2 && s1 > s2 = LT -- Look for jobs starting latest (I wanna work for the least duration :P)
-    | j1 == j2 = EQ
-    | otherwise = GT
+endFirstSel = endFirstFilter.sortBy endSort where
+  endFirstFilter (current:jobs') = current:(endFirstFilter $ baseFilter (Just current) jobs')
+  endFirstFilter [] = []
+
+jobSample = randomJobs 15 10
